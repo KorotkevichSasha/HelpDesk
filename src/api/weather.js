92ef8d4d0e76faa13dbe690d6ef786e84956d9f6
IvanKorzun <samp80268026@gmail.com>
2026-04-21 17:07:22 +0300

    feat: remove useState/useEffect, static mock data, props-based components

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,8 +1,3 @@
-/**
- * OpenWeatherMap API
- * Получи бесплатный ключ на https://openweathermap.org/api
- * и замени значение API_KEY
- */
 const API_KEY = '6c4e00b158fd99c014b628bf451a09d1';
 const BASE = 'https://api.openweathermap.org/data/2.5';
 
